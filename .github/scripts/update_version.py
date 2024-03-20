53dcbe983b613770e274bc06a0c56da08c57cac5
aceone <henols@gmail.com>
2024-03-20 14:08:31 +0100

    Build fix

--- .github/scripts/update_version.py
+++ .github/scripts/update_version.py
@@ -30,18 +30,14 @@ def update_version(major, minor, patch, date, fw_date):
             if i == 0:
                 line = m.groups(0)[0] + f" \"{major}\"\n"
                 fout.write(line)
-                print(f"Updating major version to {major}")
             elif i == 1:
                 line = m.groups(0)[0] + f" \"{minor}\"\n"
                 fout.write(line)
-                print(f"Updating minor version to {minor}")
             elif i == 2:
                 line = m.groups(0)[0] + f" \"{patch}\"\n"
                 fout.write(line)
-                print(f"Updating patch version to {patch}")
             elif i == 3:
                 line = m.groups(0)[0] + f" \"{date}\"\n"
-                
                 fout.write(line)
             elif i == 4:
                 line = m.groups(0)[0] + f" \"{major}.{minor}.{patch}\"\n"
@@ -53,9 +49,9 @@ def update_version(major, minor, patch, date, fw_date):
             fout.write(line)
 
     fout.close()
-    print(f"New versin created: {major}.{minor}.{patch}")
-
-
+    print(f"New version created: {major}.{minor}.{patch}")
+    print(f"Date: {date}")
+    print(f"FW date: {fw_date}")
 
 if __name__ == "__main__":
     update_version(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
